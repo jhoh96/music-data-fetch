2bec1cdc6f259e2eb633d618f4c172c75055d736
Justin Ji Hoon Oh <104912816+jhoh96@users.noreply.github.com>
2022-08-13 00:00:49 +0900

    Update index.js
    removed api key

--- server/index.js
+++ server/index.js
@@ -4,8 +4,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const axios = require("axios");
 const puppeteer = require("puppeteer");
-const auth_token =
-  "bvYasddC9W6qkPTUxcUA-DiTB99qCnO2E-VlSWppAGhE8XZzjTUgu4HyieBtbEAP";
 require("events").EventEmitter.prototype._maxListeners = 0;
 app.use(cors());
 app.use(express.json());
